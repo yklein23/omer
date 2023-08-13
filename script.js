acda31a862c01d70caa618df532f0e83139f88f9
yklein23 <59821187+yklein23@users.noreply.github.com>
2023-08-13 15:15:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
+<script type="module">
+  const modelViewer = document.querySelector("model-viewer");
+
+  window.switchSrc = (element, name) => {
+    const base = "../" + name;
+    modelViewer.src = base + '.gltf';
+    modelViewer.poster = base + '.webp';
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((element) => {element.classList.remove("selected");});
+    element.classList.add("selected");
+  };
+
+  document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+    // Keep slider interactions from affecting the XR scene.
+    ev.preventDefault();
+  });
+</script>
